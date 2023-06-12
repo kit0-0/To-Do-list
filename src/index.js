5059699110d4db210783cc594b0e1d2bc6f73372
kit0-0 <kt1767@gmail.com>
2023-06-12 20:52:05 +0530

    Add add and edit functionality to index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,109 @@
 import './index.css';
 
-const taskListContainer = document.getElementById('todoList');
-const tasks = [
-  {
-    description: 'complete',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'To do something',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'list tasks',
-    completed: true,
-    index: 3,
-  },
-];
+const todoListContainer = document.getElementById('todoList');
+const addBtn = document.getElementById('addBtn');
+const tasks = JSON.parse(localStorage.getItem('Tasks')) || [];
 
-export default class DisplayTasks {
-  static renderTasks() {
-    tasks.sort((a, b) => a.index - b.index);
-    taskListContainer.innerHTML = '';
-    tasks.forEach((task, index) => {
-      taskListContainer.innerHTML += `
-        <li class="task" draggable="true" data-index="${index}">
-          <div class="checkbox-container">
-            <input type="checkbox" name="${task.description}" ${task.completed ? 'checked' : ''}>
-            <input type="text" value="${task.description}" readonly>
-          </div>
-          <i class="fas fa-ellipsis-vertical" data-index="${index}"></i>
-        </li>
-      `;
+const storeTasksToLocalStorage = () => {
+  localStorage.setItem('Tasks', JSON.stringify(tasks));
+};
+
+const displayTasks = () => {
+  todoListContainer.textContent = '';
+  tasks.forEach((task, index) => {
+    todoListContainer.innerHTML += `
+      <li class="task" draggable="true" data-index="${index}">
+        <div class="checkbox-container">
+          <input type="checkbox" name="${task.description}" ${task.completed ? 'checked' : ''}>
+          <input type="text" value="${task.description}" readonly>
+        </div>
+        <i class="fas fa-ellipsis-vertical" data-index="${index}"></i>
+      </li>
+    `;
+  });
+
+  const editTask = (description, index) => {
+    tasks[index].description = description;
+    storeTasksToLocalStorage();
+  };
+
+  const addedTasks = document.querySelectorAll('.task');
+
+  const checkboxContainers = document.querySelectorAll('.task > .checkbox-container > input[type="checkbox"]');
+
+  checkboxContainers.forEach((checkbox) => {
+    const inputText = checkbox.nextElementSibling;
+    let previousState = checkbox.checked;
+
+    inputText.readOnly = true;
+
+    checkbox.addEventListener('change', (event) => {
+      const currentState = event.target.checked;
+
+      if (currentState !== previousState) {
+        const foundTask = tasks.find((task) => task.description === inputText.value);
+        if (foundTask) {
+          foundTask.completed = currentState;
+          storeTasksToLocalStorage();
+        }
+      }
+
+      previousState = currentState;
     });
-  }
-}
+  });
 
-document.addEventListener('DOMContentLoaded', () => {
-  DisplayTasks.renderTasks();
+  addedTasks.forEach((task, index) => {
+    const textInput = task.querySelector('input[type="text"]');
+    task.addEventListener('dblclick', () => {
+      textInput.readOnly = false;
+      if (task.querySelector('.fa-ellipsis-vertical')) {
+        const ellipsisIcon = task.querySelector('.fa-ellipsis-vertical');
+        ellipsisIcon.classList.remove('fa-ellipsis-vertical');
+        ellipsisIcon.classList.add('fa-trash');
+      } else {
+        const trashIcon = task.querySelector('.fa-trash');
+        trashIcon.classList.remove('fa-trash');
+        trashIcon.classList.add('fa-ellipsis-vertical');
+        textInput.readOnly = true;
+      }
+    });
+
+    // Edit
+    textInput.addEventListener('input', () => {
+      const data = textInput.value.trim();
+      editTask(data, index);
+    });
+  });
+};
+
+const addTask = () => {
+  const inputField = document.getElementById('input-task');
+  const taskDescription = inputField.value.trim();
+
+  // Check if the task description is not empty
+  if (taskDescription !== '') {
+    const newTask = {
+      description: taskDescription,
+      completed: false,
+      index: tasks.length + 1, // Set the index as array length + 1
+    };
+    tasks.push(newTask);
+    storeTasksToLocalStorage();
+    inputField.value = '';
+    displayTasks();
+  }
+};
+
+const initializeTasks = () => {
+  document.addEventListener('DOMContentLoaded', displayTasks);
+};
+
+initializeTasks();
+addBtn.addEventListener('click', addTask);
+document.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    addTask();
+  }
 });
+
+export {};
